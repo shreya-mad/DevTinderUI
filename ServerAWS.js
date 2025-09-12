e6979f72993f137bac8492b346919795300d3558
Shreya Madeshiya <smadeshiya12345@gmail.com>
2025-09-12 15:27:13 +0530

    hoising with aws done

---
+++ ServerAWS.js
@@ -0,0 +1,22 @@
+// "localhost" means your own computer.
+
+// It is a hostname (like a website address) that points back to the same device you are working on.
+
+// Technical meaning: localhost resolves to the IP address 127.0.0.1 (IPv4) or ::1 (IPv6).
+
+// Usage:
+
+// When you type http://localhost in your browser, it tells your browser to connect to a web server running on your own computer.
+
+// Developers use it for testing apps, websites, or databases on their own system before deploying them to a real server.
+
+// 👉 Example:
+// If you run a server on your computer at port 3000, you can open it in the browser by visiting:
+
+// http://localhost:3000
+
+// so to make our application or website visible to everyone we need to host our website onto some other 
+// machine so that everyone can accress that and that machine is given by AWS
+// that machine is the central place  from where we can access the website from accross the all over world
+
+
