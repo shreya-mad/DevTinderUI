9f9b843041ae6054b231a639d77977d61ff354aa
Shreya Madeshiya <smadeshiya12345@gmail.com>
2025-12-12 00:17:02 +0530

    api chnaged

--- src/api.js
+++ src/api.js
@@ -4,7 +4,9 @@ let BASE_URL = "";
 if (window.location.hostname === "localhost") {
   BASE_URL = "http://localhost:4000";
 } else {
-  BASE_URL = "https://devtinder-4-lld2.onrender.com";
+  BASE_URL = "https://devtinder-5-j3fk.onrender.com";
+  
+             
 }
 
 export  {BASE_URL};
